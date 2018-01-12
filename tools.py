95025efb253a99e5439ffdf888b281ab39a773b0
Patrick Hilhorst <philhorst@science.ru.nl>
2018-01-12 12:43:49 +0100

    Fixed the .pid file being cleared before the lock is acquired

--- tools.py
+++ tools.py
@@ -18,13 +18,20 @@ def sleep(seconds, *args, **kwargs):
 @contextmanager
 def running_once(program_name: str, per_user: bool=False):
     from fcntl import lockf, LOCK_EX, LOCK_NB, LOCK_UN
-    from os import getpid, getuid
+    from os import getpid, getuid, SEEK_SET
 
     if per_user:
         program_name += f'_{getuid()}'
 
-    with open(f'/tmp/{program_name}.pid', 'w') as f:
+    with open(f'/tmp/{program_name}.pid', 'a') as f:
         lockf(f, LOCK_EX | LOCK_NB)  # EXclusive, Non-Blocking
+
+        # Clear the file at this point.  We have acquired the lock, and since
+        # we're opening in r+ instead of w this isn't done automatically for
+        # us.  This is the right point to do it manually.
+        f.seek(0, SEEK_SET)
+        f.truncate()
+
         f.write(str(getpid()))
         f.flush()
 
